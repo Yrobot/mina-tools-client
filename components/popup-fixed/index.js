0eb42262ee0388b13bf1717b36eacc11e044df9d
Yrobot <y_robot@yeah.net>
2020-12-18 19:11:52 +0800

    更新popup

--- components/popup-fixed/index.js
+++ components/popup-fixed/index.js
@@ -10,36 +10,59 @@ Component({
     addGlobalClass: true,
   },
   properties: {
+    backgroundColor: {
+      type: String,
+      value: '',
+    },
     selector: {
       type: String,
       value: '',
     },
-    bgClose: {
+    tapBgClose: {
+      type: Boolean,
+      value: true,
+    },
+    scrollBgClose: {
+      type: Boolean,
+      value: false,
+    },
+    catchScroll: {
       type: Boolean,
       value: true,
     },
   },
-  observers: {
-    selector(selector) {
-      if (selector) {
-        this.doPopupPosition(selector);
-      }
-    },
-  },
+  observers: {},
   data: {
-    positionStyle: 'left:0;top:0;',
+    node: {
+      left: 0,
+      top: 0,
+      width: 0,
+      height: 0,
+    },
+    viewport: {
+      width: 0,
+      height: 0,
+    },
     visible: false,
   },
   lifetimes: {
     ready() {
-      this.doPopupPosition(this.data.selector);
+      this.getViewport().then(({ viewport }) => {
+        this.setData({ viewport });
+      });
     },
   },
   methods: {
-    catchTouchMove() {},
+    touchMove() {
+      if (this.data.scrollBgClose) {
+        this._close();
+      }
+    },
     _open() {
-      this.setData({
-        visible: true,
+      this.beforeOpen(this.data.selector).then(() => {
+        this.setData({
+          visible: true,
+        });
       });
     },
     _close() {
@@ -48,35 +71,59 @@ Component({
       });
     },
     tapBG() {
-      if (this.data.bgClose) {
+      if (this.data.tapBgClose) {
         this._close();
       }
     },
-    setPosition({ left = 0, top = 0 }) {
-      this.setData({
-        positionStyle: `left:${left}px;top:${top}px;`,
+    beforeOpen(selector) {
+      const { width = 0 } = this.data.viewport;
+      const needGetViewport = width === 0;
+      return Promise.all(needGetViewport ? [this.getNode(selector), this.getViewport()] : [this.getNode(selector)])
+        .then((res = []) => {
+          var data = {};
+          res.map((d) => {
+            data = { ...data, ...d };
+          });
+          this.setData(data, () => {
+            this.triggerEvent('position', {
+              node: this.data.node,
+              viewport: this.data.viewport,
+            });
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    getViewport() {
+      return new Promise((resolve) => {
+        const query = wx.createSelectorQuery();
+        query
+          .selectViewport()
+          .boundingClientRect(({ height, width }) => {
+            resolve({
+              viewport: { height, width },
+            });
+          })
+          .exec();
       });
     },
-    doPopupPosition(selector) {
-      if (selector) {
-        const query = wx.createSelectorQuery();
-        query.select(selector).boundingClientRect();
-        query.selectViewport().scrollOffset();
-        query.exec((res) => {
-          const [node, viewport] = res || [];
-          if (!node || !viewport) {
-            console.error(`popup-fixed 更新组件位置时无法获取位置信息`, {
-              node,
-              viewport,
-            });
-            return;
-          }
-          this.setPosition({
-            left: node.left + node.width / 2,
-            top: node.top + node.height,
-          });
-        });
-      }
+    getNode(selector) {
+      return new Promise((resolve, reject) => {
+        if (selector) {
+          const query = wx.createSelectorQuery();
+          query
+            .select(selector)
+            .boundingClientRect(({ left, top, height, width }) => {
+              resolve({
+                node: { left, top, height, width },
+              });
+            })
+            .exec();
+        } else {
+          reject('getNode() 没有 selector');
+        }
+      });
     },
   },
 });
--- components/popup-window/index.js
+++ components/popup-window/index.js
@@ -1,7 +1,12 @@
 Component({
   behaviors: ['wx://component-export'],
   export() {
-    return this.selectComponent('#popup-fixed');
+    this.popupFixed = this.selectComponent('#popup-fixed');
+    return this.popupFixed;
+  },
+  popupFixed: {
+    open() {},
+    close() {},
   },
   options: {
     addGlobalClass: true,
@@ -11,15 +16,63 @@ Component({
       type: String,
       value: '',
     },
-    bgClose: {
+    tapBgClose: {
+      type: Boolean,
+      value: true,
+    },
+    scrollBgClose: {
+      type: Boolean,
+      value: false,
+    },
+    visible: {
+      type: Boolean,
+      value: undefined,
+    },
+    backgroundColor: {
+      type: String,
+      value: '',
+    },
+    catchScroll: {
       type: Boolean,
       value: true,
     },
   },
-  observers: {},
-  data: {},
+  observers: {
+    visible(visible) {
+      if (visible === undefined) return;
+      // 利用setData保证this.popupFixed初始化完成
+      this.setData({}, () => {
+        if (visible) {
+          this.popupFixed.open();
+        } else {
+          this.popupFixed.close();
+        }
+      });
+    },
+  },
+  data: {
+    isLeft: true,
+    isTop: true,
+    nodeHalfHeight: 0,
+  },
   lifetimes: {
     ready() {},
   },
-  methods: {},
+  methods: {
+    doDirection(e) {
+      const { node, viewport } = e.detail || {};
+      if (node && viewport) {
+        const { left, top } = node;
+        const isLeft = left < viewport.width / 2;
+        const isTop = top < viewport.height / 2;
+        this.setData({
+          isLeft,
+          isTop,
+          nodeHalfHeight: node.height / 2,
+        });
+      } else {
+        console.error('popup-window doDirection 还未获取到 nodeHalfHeight, left, top, viewport');
+      }
+    },
+  },
 });
